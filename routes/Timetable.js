c4b8415eb8c4eb6a8903f81a904e40390f9bb5cb
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-04-16 09:39:23 +0530

    Update Timetable.js

--- routes/Timetable.js
+++ routes/Timetable.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const checkAuth = require("../middlewares/checkAuth");
+const { addTimeTable, getAllTimetable } = require("../controllers/Timetable");
 
 const router = express.Router()
 
-
+router.post("/new", checkAuth, addTimeTable)
+router.get("/all", getAllTimetable)
 
 module.exports = router;
